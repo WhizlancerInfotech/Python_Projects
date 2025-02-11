c034c02da3282fbb77eee297081003c8f5b60365
WhizlancerInfotech <developer@whizlancer.com>
2025-02-11 05:43:08 -0800

    Delete Email.py

--- Email.py
+++
@@ -1,9 +0,0 @@
-import smtplib
-
-server = smtplib.SMTP('smtp.gmail.com', 587)
-server.starttls()
-server.login("your_email@gmail.com", "your_password")
-
-message = "Hello, this is an automated email!"
-server.sendmail("your_email@gmail.com", "receiver_email@gmail.com", message)
-server.quit()
