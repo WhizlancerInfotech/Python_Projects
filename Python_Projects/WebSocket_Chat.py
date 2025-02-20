6c4a133bf743e9e5b8184324d43d8067f3b34e08
WhizlancerInfotech <developer@whizlancer.com>
2025-02-20 12:27:43 +0000

    Python_Projects

---
+++ Python_Projects/WebSocket_Chat.py
@@ -0,0 +1,17 @@
+import asyncio
+import websockets
+
+clients = set()
+
+async def handler(websocket, path):
+    clients.add(websocket)
+    try:
+        async for message in websocket:
+            for client in clients:
+                await client.send(message)
+    finally:
+        clients.remove(websocket)
+
+server = websockets.serve(handler, "localhost", 8765)
+asyncio.get_event_loop().run_until_complete(server)
+asyncio.get_event_loop().run_forever()
