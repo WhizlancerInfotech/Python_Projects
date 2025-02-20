7f3ad4537967859d8c2a27fcc166d9bc98122725
WhizlancerInfotech <developer@whizlancer.com>
2025-02-20 12:28:20 +0000

    PYthon_Projects

---
+++ Python_Projects/Sudoku_Solver.py
@@ -0,0 +1,18 @@
+def is_valid(board, r, c, num):
+    for i in range(9):
+        if board[r][i] == num or board[i][c] == num:
+            return False
+    return True
+
+def solve(board):
+    for r in range(9):
+        for c in range(9):
+            if board[r][c] == 0:
+                for num in range(1, 10):
+                    if is_valid(board, r, c, num):
+                        board[r][c] = num
+                        if solve(board):
+                            return True
+                        board[r][c] = 0
+                return False
+    return True
