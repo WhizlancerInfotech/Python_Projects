bdd832dba852611401ff1c8551d4386ae39aff0c
WhizlancerInfotech <developer@whizlancer.com>
2025-03-17 04:55:49 -0700

    Create Logging_Middleware.py

---
+++ Django_Projects/Logging_Middleware.py
@@ -0,0 +1,10 @@
+import logging
+logger = logging.getLogger(__name__)
+
+class LoggingMiddleware:
+    def __init__(self, get_response):
+        self.get_response = get_response
+
+    def __call__(self, request):
+        logger.info(f"Request received: {request.path}")
+        return self.get_response(request)
